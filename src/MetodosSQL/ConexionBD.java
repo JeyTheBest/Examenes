76de994132cdb2ea53d2f3d71af9f165d2125ebd
JeyTheBest <javiersatian369@hotmail.com>
2022-09-20 20:10:03 -0500

    Version 1.0

---
+++ src/MetodosSQL/ConexionBD.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MetodosSQL;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author javie
+ */
+public class ConexionBD {
+    
+    
+    public static Connection conectar(){
+        Connection conexion = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/juegos","root","");
+            System.out.println("conexion establecidad");
+        }catch(ClassNotFoundException | SQLException e ){
+            System.out.println(e);
+        }
+        return conexion;
+    }
+    
+}
---
+++ src/examenes/Examenes.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenes;
+
+/**
+ *
+ * @author javie
+ */
+public class Examenes {
+
+    
+    public static void main(String[] args) {
+        FrmLogin v = new FrmLogin();
+        v.setVisible(true);
+    }
+    
+}
